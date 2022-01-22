bb18038001eb24b8cd58a656e8d4148a6d8be86f
peyman <mohammad.hakemi.far@gmail.com>
2022-01-22 12:25:28 +0330

    added typescript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        root: './src',
+      },
+    ],
+    'jest-hoist',
+    'react-native-reanimated/plugin',
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'], //removing consoles.log from app during release (production) versions
+    },
+  },
 };
